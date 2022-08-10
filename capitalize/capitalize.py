080e262ea32d2907797686081df7a73242f3879f
Antesser <ahriman.kun@gmail.com>
2022-08-10 16:27:22 +0300

    Changed capitalize.py

--- capitalize/capitalize.py
+++ capitalize/capitalize.py
@@ -1,7 +1,6 @@
 def capitalize(text):
     if text == '':
         return ''
-    print(1)
     first_char = text[0].upper()
     rest_substring = text[1:]
     return f'{first_char}{rest_substring}'
